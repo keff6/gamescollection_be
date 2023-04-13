d2d1ceb86fbd7286bc1307f61d6c05082745918f
keff6 <kev.fallas@gmail.com>
2023-04-13 11:10:28 -0600

    Add manufacturer CRUD functions

---
+++ src/services/manufacturers.js
@@ -0,0 +1,67 @@
+const dbConnection = require("../utils/db");
+const { v4: uuidv4 } = require('uuid');
+
+class ManufacturersService {
+  /**
+   *  ADD MANUFACTURER
+   */
+  async add(manufacturerObj) {
+const insertQuery =  `INSERT INTO manufacturer(id, name, origin) values('${uuidv4()}', '${manufacturerObj.name}', '${manufacturerObj.origin || ""}')`;
+    
+    try {
+      await dbConnection.query(insertQuery);
+      return "Added succesfully!";
+    } catch(err) {
+      throw new Error(err.messsage);
+    } 
+    
+  }
+
+  /**
+   *  UPDATE MANUFACTURER
+   */
+  async update(manufacturerId, manufacturerObj) {
+    const updateQuery = `UPDATE manufacturer SET name = '${manufacturerObj.newName}', origin = '${manufacturerObj.newOrigin || ""}' where id = '${manufacturerId}'`;
+
+    try {
+      await dbConnection.query(updateQuery);
+      return "Updated succesfully!";
+    } catch(err) {
+      throw new Error(err.messsage);
+    } 
+  }
+
+  /**
+   *  REMOVE MANUFACTURER
+   */
+  async remove(manufacturerId) {
+    const removeQuery = `DELETE FROM manufacturer where id = '${manufacturerId}'`;
+
+    // TODO: sub routine to also delete all lines on game_x_genre table
+
+    try {
+      await dbConnection.query(removeQuery);
+      return "Removed succesfully!";
+    } catch(err) {
+      throw new Error(err.messsage);
+    } 
+  }
+  
+  /**
+   *  GET ALL MANUFACTURERS
+   */
+  async getAll() {
+    const selectQuery = 'SELECT * FROM manufacturer';
+
+    try {
+      const rows = await dbConnection.query(selectQuery);
+      return rows;
+    } catch(err) {
+      throw new Error(err.messsage);
+    } 
+    
+  }
+
+}
+
+module.exports = ManufacturersService
