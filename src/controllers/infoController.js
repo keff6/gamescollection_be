4eb28d920c17292fec32b9dccefc3d6da82d4f3b
Kevin Fallas <kev.fallas@gmail.com>
2025-10-19 11:51:04 -0600

    Add info routes

--- src/controllers/infoController.js
+++ src/controllers/infoController.js
@@ -13,7 +13,78 @@ const getTotals = async (req, res, next) => {
   }
 }
 
+/**
+ *  GET TOTAL GAMES BY CONSOLE
+ */
+const getTotalGamesByConsole = async (req, res, next) => {
+  try {
+    const infoService = new InfoService()
+    const totalGamesByConsole = await infoService.getTotalGamesByConsole()
+    res.send(totalGamesByConsole)
+  } catch(error) {
+    next(error)
+  }
+}
+
+/**
+ *  GET LATEST 5 GAME ADDITIONS
+ */
+const getLatestAdditions = async (req, res, next) => {
+  try {
+    const infoService = new InfoService()
+    const latestAdditions = await infoService.getLatestAdditions()
+    res.send(latestAdditions)
+  } catch(error) {
+    next(error)
+  }
+}
+
+/**
+ *  GET CURENT PLAYING GAMES
+ */
+const getPlayingGames = async (req, res, next) => {
+  try {
+    const infoService = new InfoService()
+    const playingGames = await infoService.getPlayingGames()
+    res.send(playingGames)
+  } catch(error) {
+    next(error)
+  }
+}
+
+/**
+ *  GET GENNRES DISTRIBUTION
+ */
+const getGenresDistribution = async (req, res, next) => {
+  try {
+    const infoService = new InfoService()
+    const genresDist = await infoService.getGenresDistribution()
+    res.send(genresDist)
+  } catch(error) {
+    next(error)
+  }
+}
+
+/**
+ *  GET GAMES BY CONDITION
+ */
+const getGamesByCondition = async (req, res, next) => {
+  try {
+    const infoService = new InfoService()
+    const gamesByCondition = await infoService.getGamesByCondition()
+    res.send(gamesByCondition)
+  } catch(error) {
+    next(error)
+  }
+}
+
+
 
 module.exports = {
   getTotals,
+  getTotalGamesByConsole,
+  getLatestAdditions,
+  getPlayingGames,
+  getGenresDistribution,
+  getGamesByCondition,
 }
--- src/services/info.js
+++ src/services/info.js
@@ -20,6 +20,99 @@ class InfoService {
     } 
     
   }
+
+  /**
+   *  GET TOTAL GAMES BY CONSOLE
+   */
+  async getTotalGamesByConsole() {
+    const selectQuery = `
+      SELECT c.id, b.name, c.name, count(0) as total_games
+      FROM game AS g, console AS c, brand AS b
+      WHERE g.id_console = c.id AND c.id_brand = b.id
+      GROUP BY c.id`;
+
+    try {
+      const totalGamesByConsole = await dbConnection.query(selectQuery);
+      return totalGamesByConsole;
+    } catch(err) {
+      throw new Error(err);
+    } 
+  }
+
+  /**
+   *  GET LATEST 5 GAMES ADDED
+   */
+  async getLatestAdditions() {
+    const selectQuery = `
+      SELECT c.name, g.title, g.year
+      FROM logs as l, game as g, console as c
+      WHERE action = 'INSERT' AND l.record_id = g.id AND g.id_console = c.id
+      ORDER BY created_at DESC
+      LIMIT 5`;
+
+    try {
+      const latestAdditions = await dbConnection.query(selectQuery);
+      return latestAdditions;
+    } catch(err) {
+      throw new Error(err);
+    } 
+  }
+
+  /**
+   *  GET CURRENT PLAYING GAMES
+   */
+  async getPlayingGames() {
+    const selectQuery = `
+      SELECT id, title FROM game WHERE is_playing = 1`;
+
+    try {
+      const playingGames = await dbConnection.query(selectQuery);
+      return playingGames;
+    } catch(err) {
+      throw new Error(err);
+    } 
+  }
+
+  /**
+   *  GET GENRES DISTRIBUTION
+   */
+  async getGenresDistribution() {
+    const selectQuery = `
+      SELECT gen.name, count(0) AS total
+      FROM game_x_genre AS gxg, genre AS gen
+      WHERE gxg.id_genre = gen.id
+      GROUP BY gen.id
+      ORDER BY total DESC`;
+
+    try {
+      const genresDist = await dbConnection.query(selectQuery);
+      return genresDist;
+    } catch(err) {
+      throw new Error(err);
+    } 
+  }
+
+  /**
+   *  GET GAMES BY CONDITION
+   */
+  async getGamesByCondition() {
+    const selectQuery = `
+      SELECT
+        SUM(is_new) as new,
+        SUM(is_complete) as complete,
+        SUM(is_digital) AS digital,
+        (count(0) - (SUM(is_new) + SUM(is_complete) + SUM(is_digital))) AS incomplete
+      FROM game`;
+
+    try {
+      const gamesByCondition= await dbConnection.query(selectQuery);
+      return gamesByCondition;
+    } catch(err) {
+      throw new Error(err);
+    } 
+  }
+
+  
 }
 
 module.exports = InfoService
